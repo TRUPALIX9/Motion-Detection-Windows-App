061649e4d25caf081cf0cf9fcb2122523883da0c
Trupal <trupalp313@gmail.com>
2023-12-21 18:05:19 +0530

    removed unneccesary things

--- FFMPEG-Stream-Forwarding/FFmpeg Stream Forwarding.cs
+++ FFMPEG-Stream-Forwarding/FFmpeg Stream Forwarding.cs
@@ -1,4 +1,3 @@
-using NetFwTypeLib;
 
 namespace FFMPEG_Stream_Forwarding
 {
@@ -12,9 +11,6 @@ namespace FFMPEG_Stream_Forwarding
             txt_input.Text = "rtsp://aivid:aivid_2022@192.168.111.105:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif";
             txt_output.Text = "rtsp://cloud.aividtechvision.com:8556/aivid50";
             string executablePath = "\"C:\\Users\\Aivid11\\source\\repos\\TRUPALIX9\\Motion-Detection-Windows-App\\FFMPEG-Stream-Forwarding\\bin\\Debug\\net6.0-windows\\FFMPEG-Stream-Forwarding.exe\"";
-            AddFirewallRule("MyApp Inbound", executablePath, NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_IN);
-                        // Create outbound rule
-            AddFirewallRule("MyApp Outbound", executablePath, NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT);
         }
 
         private void btn_start_Click( object sender, EventArgs e )
@@ -28,29 +24,6 @@ namespace FFMPEG_Stream_Forwarding
             _class1.StopCapture();
             btn_start.Enabled = true;
 
-        }
-        static void AddFirewallRule( string ruleName, string programPath, NET_FW_RULE_DIRECTION_ direction )
-        {
-            try
-            {
-                Type typeNetFwPolicy2 = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
-                INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(typeNetFwPolicy2);
-
-                Type typeNetFwRule = Type.GetTypeFromProgID("HNetCfg.FWRule");
-                INetFwRule firewallRule = (INetFwRule)Activator.CreateInstance(typeNetFwRule);
-
-                firewallRule.Name = ruleName;
-                firewallRule.ApplicationName = programPath;
-                firewallRule.Direction = direction;
-                firewallRule.Enabled = true;
-
-                // Add the rule to the firewall
-                fwPolicy2.Rules.Add(firewallRule);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error adding firewall rule: {ex.Message}");
-            }
         }
         private void btn_clearText_Click( object sender, EventArgs e )
         {
